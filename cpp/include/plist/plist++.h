44b04532705d9919678a02fb9a2f5f20a6713e11
Jackson Coxson <jkcoxson@gmail.com>
2025-08-15 12:04:40 -0600

    Add cpp library

---
+++ cpp/include/plist/plist++.h
@@ -0,0 +1,39 @@
+/*
+ * plist++.h
+ * Main include of libplist C++ binding
+ *
+ * Copyright (c) 2009 Jonathan Beck All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef LIBPLISTXX_H
+#define LIBPLISTXX_H
+
+#include <plist/Array.h>
+#include <plist/Boolean.h>
+#include <plist/Data.h>
+#include <plist/Date.h>
+#include <plist/Dictionary.h>
+#include <plist/Integer.h>
+#include <plist/Key.h>
+#include <plist/Node.h>
+#include <plist/Real.h>
+#include <plist/String.h>
+#include <plist/Structure.h>
+#include <plist/Uid.h>
+#include <plist/plist.h>
+
+#endif
